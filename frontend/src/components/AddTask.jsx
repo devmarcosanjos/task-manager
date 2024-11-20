0255b7d4789788b22bcd4fe3b5f756198588207f
Marcos <dev.marcosanjos@gmail.com>
2024-11-20 15:13:33 -0300

    feat: integrate react-alert for task addition notifications in AddTask component

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -3,12 +3,25 @@ import "./AddTask.scss";
 import { useState } from "react";
 import CustomButton from "./CustomButon";
 import { FaPlus } from "react-icons/fa";
+import { useAlert } from "react-alert";
 
 const AddTask = () => {
   const [task, setTask] = useState("");
 
+  const alert = useAlert();
+
   const onChange = (e) => {
     setTask(e.target.value);
+  };
+
+  const handleTaskAddition = async () => {
+    try {
+      if (task.length === 0) {
+        return alert.error("Por favor, adicione uma tarefa");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -18,7 +31,7 @@ const AddTask = () => {
         value={task}
         onChange={onChange}
       />
-      <CustomButton onClick={() => console.log(task)}>
+      <CustomButton onClick={handleTaskAddition}>
         <FaPlus size={14} color="#FFF" />
       </CustomButton>
     </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,26 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// optional configuration
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 2000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE,
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>
 );
 
